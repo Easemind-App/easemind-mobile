da52d66f1857549ae9f3382bd6405166fe8b288a
ririnzakiyah02 <ririnzakiyah02@Gmail.com>
2024-06-08 17:39:45 +0700

    feature: ui homepage, profile,edit profile

--- app/src/main/java/com/example/easemind/JournalActivity.kt
+++ app/src/main/java/com/example/easemind/JournalActivity.kt
@@ -3,33 +3,36 @@ package com.example.easemind
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
-
+class JournalActivity : AppCompatActivity() {
     lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_journal)
 
         bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
-        bottomNavigationView.selectedItemId = R.id.home
+        bottomNavigationView.selectedItemId = R.id.journal
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.home -> {
-                    true
-                }
-                R.id.journal -> {
-                    val intent = Intent(this, JournalActivity::class.java)
+                    val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent, ActivityOptions.makeCustomAnimation(this, 0, 0).toBundle())
                     finish()
                 }
+
                 R.id.profile -> {
                     val intent = Intent(this, ProfileActivity::class.java)
                     startActivity(intent, ActivityOptions.makeCustomAnimation(this, 0, 0).toBundle())
                     finish()
+                }
+                R.id.journal -> {
+                    true
                 }
             }
             true
--- app/src/main/java/com/example/easemind/ProfileActivity.kt
+++ app/src/main/java/com/example/easemind/ProfileActivity.kt
@@ -3,37 +3,51 @@ package com.example.easemind
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
 
     lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
+
+        val fragmentManager = supportFragmentManager
+        val profileFragment = ProfileFragment()
+        val fragment = fragmentManager.findFragmentByTag(ProfileFragment::class.java.simpleName)
+
+        if (fragment !is ProfileFragment) {
+            Log.d("MyFragment", "Fragment Name :" + ProfileFragment::class.java.simpleName)
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.container, profileFragment, ProfileFragment::class.java.simpleName)
+                .commit()
+        }
 
         bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
-        bottomNavigationView.selectedItemId = R.id.home
+        bottomNavigationView.selectedItemId = R.id.profile
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.home -> {
-                    true
-                }
                 R.id.journal -> {
                     val intent = Intent(this, JournalActivity::class.java)
                     startActivity(intent, ActivityOptions.makeCustomAnimation(this, 0, 0).toBundle())
                     finish()
                 }
-                R.id.profile -> {
-                    val intent = Intent(this, ProfileActivity::class.java)
+                R.id.home -> {
+                    val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent, ActivityOptions.makeCustomAnimation(this, 0, 0).toBundle())
                     finish()
+                }
+                R.id.profile -> {
+                    true
                 }
             }
             true
         }
 
     }
+
 }
