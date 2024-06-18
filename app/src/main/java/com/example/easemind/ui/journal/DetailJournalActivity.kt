3f9f0146ccfcdf84513f463fcc7d5ac443202b22
kayleerudaina <kayleerudaina@gmail.com>
2024-06-19 02:07:23 +0700

    fix: add journal item

--- app/src/main/java/com/example/easemind/ui/journal/DetailJournalActivity.kt
+++ app/src/main/java/com/example/easemind/ui/journal/DetailJournalActivity.kt
@@ -5,12 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.easemind.R
+import com.example.easemind.databinding.ActivityDetailJournalBinding
 
 class DetailJournalActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailJournalBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_journal)
+        binding = ActivityDetailJournalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        Glide.with(this).load(intent.getStringExtra(EXTRA_EMOTION)).into(binding.resultEmoji)
+        binding.result.text = intent.getStringExtra(EXTRA_CATEGORY)
+        binding.dateOfJournal.text = intent.getStringExtra(EXTRA_DATE)
+        binding.thoughtValue.text = intent.getStringExtra(EXTRA_THOUGHTS)
+    }
+
+    companion object {
+        const val EXTRA_EMOTION = "extra_emotion"
+        const val EXTRA_CATEGORY = "extra_category"
+        const val EXTRA_DATE = "extra_date"
+        const val EXTRA_THOUGHTS = "extra_thoughts"
     }
 }
