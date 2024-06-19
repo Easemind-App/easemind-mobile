b437a603aa1b6e8826d13f79b7c535a7117e5064
kayleerudaina <kayleerudaina@gmail.com>
2024-06-19 11:22:40 +0700

    fix: modify journal data adapter

--- app/src/main/java/com/example/easemind/ui/journal/JournalActivity.kt
+++ app/src/main/java/com/example/easemind/ui/journal/JournalActivity.kt
@@ -3,17 +3,23 @@ package com.example.easemind.ui.journal
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.easemind.R
 import com.example.easemind.data.response.JournalsItem
 import com.example.easemind.databinding.ActivityJournalBinding
+import com.example.easemind.ui.authentication.AuthenticationViewModel
+import com.example.easemind.ui.authentication.AuthenticationViewModelFactory
 import com.example.easemind.ui.homepage.MainActivity
 import com.example.easemind.ui.profile.ProfileActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class JournalActivity : AppCompatActivity() {
-
+    private val authenticationViewModel by viewModels<AuthenticationViewModel> {
+        AuthenticationViewModelFactory.getInstance(this)
+    }
     private lateinit var binding: ActivityJournalBinding
     lateinit var bottomNavigationView: BottomNavigationView
 
@@ -24,6 +30,17 @@ class JournalActivity : AppCompatActivity() {
 
         val layoutManager = LinearLayoutManager(this)
         binding.rvJournal.layoutManager = layoutManager
+
+        authenticationViewModel.getSession().observe(this) { user ->
+
+            authenticationViewModel.getJournal(user.token)
+            authenticationViewModel.journal.observe(this) {
+                setJournal(it)
+            }
+            authenticationViewModel.isLoading.observe(this) {
+                showLoading(it)
+            }
+        }
 
         bottomNavigationView = binding.bottomNavView
         bottomNavigationView.selectedItemId = R.id.journal
@@ -53,6 +70,10 @@ class JournalActivity : AppCompatActivity() {
         val adapter = JournalAdapter()
         adapter.submitList(journals)
         binding.rvJournal.adapter = adapter
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility  = if (isLoading) View.VISIBLE else View.GONE
     }
 
 }
