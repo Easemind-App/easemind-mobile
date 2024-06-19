50960669496cc16ec523424b7b70c66f98c6a22b
kayleerudaina <kayleerudaina@gmail.com>
2024-06-19 10:22:29 +0700

    fix: fix journal adapter

--- app/src/main/java/com/example/easemind/ui/journal/JournalActivity.kt
+++ app/src/main/java/com/example/easemind/ui/journal/JournalActivity.kt
@@ -4,7 +4,9 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.easemind.R
+import com.example.easemind.data.response.JournalsItem
 import com.example.easemind.databinding.ActivityJournalBinding
 import com.example.easemind.ui.homepage.MainActivity
 import com.example.easemind.ui.profile.ProfileActivity
@@ -19,6 +21,9 @@ class JournalActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityJournalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvJournal.layoutManager = layoutManager
 
         bottomNavigationView = binding.bottomNavView
         bottomNavigationView.selectedItemId = R.id.journal
@@ -43,4 +48,11 @@ class JournalActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun setJournal(journals: List<JournalsItem>) {
+        val adapter = JournalAdapter()
+        adapter.submitList(journals)
+        binding.rvJournal.adapter = adapter
+    }
+
 }
