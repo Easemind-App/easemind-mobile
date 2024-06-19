d05e96be6c20791d3065269f69972ace116a6481
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-06-19 18:05:32 +0700

    Revert "Added authentication (beta)"
    This reverts commit 4014b90361f834d05d85c86c8231f7f3b17ddbbb.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-
 }
 
 android {
@@ -37,17 +36,13 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.play.services.base)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    // Reference to Google Play services Auth
-    implementation(libs.google.play.services.auth)
 }
-
