09a6e0ebad341dff24d52b62ae3ffdb2d8eb68ff
kayleerudaina <kayleerudaina@gmail.com>
2024-06-21 12:22:15 +0700

    feat: added logout button for authentication

--- app/src/main/java/com/example/easemind/ui/profile/profileFragment/ProfileFragment.kt
+++ app/src/main/java/com/example/easemind/ui/profile/profileFragment/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.easemind.ui.profile.profileFragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,9 +13,13 @@ import com.bumptech.glide.Glide
 import com.example.easemind.R
 import com.example.easemind.data.response.UserResponse
 import com.example.easemind.databinding.FragmentProfileBinding
+import com.example.easemind.ui.authentication.AuthenticationActivity
 import com.example.easemind.ui.authentication.AuthenticationViewModel
 import com.example.easemind.ui.authentication.AuthenticationViewModelFactory
 import com.example.easemind.ui.profile.editProfileFragment.EditProfileFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class ProfileFragment : Fragment() {
 
@@ -25,6 +30,7 @@ class ProfileFragment : Fragment() {
     private val authenticationViewModel: AuthenticationViewModel by viewModels {
         AuthenticationViewModelFactory.getInstance(requireContext())
     }
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,8 +43,21 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
         setupViews()
         observeViewModel()
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(requireActivity()) {
+                val intent = Intent(activity, AuthenticationActivity::class.java)
+                startActivity(intent)
+            }
     }
 
     private fun setupViews() {
@@ -60,6 +79,10 @@ class ProfileFragment : Fragment() {
                 addToBackStack(null)
                 commit()
             }
+        }
+
+        binding.btnLogout.setOnClickListener {
+            signOut()
         }
     }
 
